c3c635d0753f87efd514f436f32900370e1a11d7
superxin <liuxin2533@gmail.com>
2025-05-07 16:11:32 +0800

    sitemap url 生成逻辑修改

--- server/api/__sitemap__/urls.ts
+++ server/api/__sitemap__/urls.ts
@@ -5,7 +5,7 @@ export default defineSitemapEventHandler(async () => {
   const games = await getGames()
   return games.map((g) => {
     return {
-      loc: `/games/${g.id}`,
+      loc: `/games/${g.id.replace(/\s/g, '-').toLowerCase()}`,
       images: [
         {
           loc: g.cover,
